4ed913a13765ebfc79d40a856463a2c7e695db4c
Anze Slosar <slosar@gmail.com>
2016-04-27 11:51:23 +0100

    MPI support

--- fastcat_CoLore/mkcat.py
+++ fastcat_CoLore/mkcat.py
@@ -7,6 +7,8 @@ import numpy as np
 from optparse import OptionParser
 import datetime
 import os
+from mpi4py import MPI
+
 
 #default in path
 ipath="/project/projectdirs/lsst/LSSWG/colore_raw"
@@ -51,51 +53,59 @@ bz=np.genfromtxt(o.ipath+'/bz.txt', dtype=None, names=["z","bz"])
 dNdz=np.genfromtxt(o.ipath+'/Nz.txt', dtype=None, names=["z","dNdz"])
 fopath=None
 
+comm = MPI.COMM_WORLD
+mrank = comm.Get_rank()
+mranks = "["+str(mrank)+"]:"
+msize = comm.Get_size()
+if (mrank==0):
+    print "MPI Size:", msize
+    
 for i in range(o.Nstart,o.Nr):
-    print "Reading set ",i
-    gals,inif=readColore(o.ipath+"/Set%i"%i)
-    print len(gals)," galaxies read."
-    if (len(gals)==0):
-        print "No galaxies!"
-        stop()
+    if (i%msize==mrank):
+        print mranks, "Reading set ",i
+        gals,inif=readColore(o.ipath+"/Set%i"%i)
+        print mranks, len(gals)," galaxies read."
+        if (len(gals)==0):
+            print mranks, "No galaxies!"
+            stop()
 
-    # start catalog with known dNdz and bz
-    N=len(gals)
-    meta={}
-    for k,v in inif.items():
-        meta['colore_'+k]=v
-    meta['mkcat_git_version']=get_git_revision_short_hash()
-    meta['timestamp']=str(datetime.datetime.now())
-    meta['realspace']=o.realspace
-    cat=fc.Catalog(N, fields=['ra','dec','z'],dNdz=dNdz, bz=bz,
-                    meta=meta)
-    cat['ra']=gals['RA']
-    cat['dec']=gals['DEC']
-    if (o.realspace):
-        cat['z']=gals['Z_COSMO']
-    else:
-        cat['z']=gals['Z_COSMO']+gals['DZ_RSD']
+        # start catalog with known dNdz and bz
+        N=len(gals)
+        meta={}
+        for k,v in inif.items():
+            meta['colore_'+k]=v
+        meta['mkcat_git_version']=get_git_revision_short_hash()
+        meta['timestamp']=str(datetime.datetime.now())
+        meta['realspace']=o.realspace
+        cat=fc.Catalog(N, fields=['ra','dec','z'],dNdz=dNdz, bz=bz,
+                        meta=meta)
+        cat['ra']=gals['RA']
+        cat['dec']=gals['DEC']
+        if (o.realspace):
+            cat['z']=gals['Z_COSMO']
+        else:
+            cat['z']=gals['Z_COSMO']+gals['DZ_RSD']
 
-    ## first apply window function
-    print "Creating window..."
-    wfunc=getWindowFunc(o)
-    print "Applying window..."
-    cat.setWindow(wfunc, apply_to_data=True)
-    ## next apply photoz
-    print "Applying photoz..."
-    cat.setPhotoZ(fc.photoz.PhotoZGauss(0.01),apply_to_data=True)
-    ## now create full output path
-    if fopath is None:
-        dt=datetime.datetime.now()
-        daystr="%02i%02i%02i"%(dt.year-2000,dt.month,dt.day)
-        fopath=o.opath+"/"+daystr+"+"+cat.photoz.NameString()+"+"+cat.window.NameString()
-        if len(o.oextra)>0:
-            fopath+="+"+o.oextra
-        if not os.path.exists(fopath):
-            os.makedirs(fopath)
-    ## write the actual catalog
-    fname=fopath+'/catalog%i.h5'%(i)
-    print "Writing "+fname+" ..."
-    cat.writeH5(fname)
+        ## first apply window function
+        print mranks, "Creating window..."
+        wfunc=getWindowFunc(o)
+        print mranks, "Applying window..."
+        cat.setWindow(wfunc, apply_to_data=True)
+        ## next apply photoz
+        print mranks, "Applying photoz..."
+        cat.setPhotoZ(fc.photoz.PhotoZGauss(0.01),apply_to_data=True)
+        ## now create full output path
+        if fopath is None:
+            dt=datetime.datetime.now()
+            daystr="%02i%02i%02i"%(dt.year-2000,dt.month,dt.day)
+            fopath=o.opath+"/"+daystr+"+"+cat.photoz.NameString()+"+"+cat.window.NameString()
+            if len(o.oextra)>0:
+                fopath+="+"+o.oextra
+            if not os.path.exists(fopath):
+                os.makedirs(fopath)
+        ## write the actual catalog
+        fname=fopath+'/catalog%i.h5'%(i)
+        print mranks, "Writing "+fname+" ..."
+        cat.writeH5(fname)
 
-
+comm.Barrier()
