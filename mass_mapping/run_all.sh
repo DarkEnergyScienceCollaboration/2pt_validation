5e0c82208b4d48991a7ad8161232a714a654c1b6
fjaviersanchez <francs1@uci.edu>
2017-04-04 20:35:26 -0700

    Updated batch file

--- mass_mapping/run_all.sh
+++ mass_mapping/run_all.sh
@@ -1,11 +1,13 @@
 #!/bin/bash -l
 #SBATCH --partition debug
-#SBATCH --nodes 32
-#SBATCH --time=00:29:59
+####SBATCH --qos premium
+#SBATCH --nodes 64
+#SBATCH --time=00:30:00
 #SBATCH --job-name=CoLoRe_WL
-#SBATCH --account=m1727
+#SBATCH --account=des
 #SBATCH -C haswell
 #module load gsl
 #module load fftw
-srun -n 256 ./CoLoRe param_colore.cfg 
+export OMP_NUM_THREADS=64
+srun -n 64 -c 64 /global/homes/j/jsanch87/CoLoRe/CoLoRe param_hsc.cfg 
 
